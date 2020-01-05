b5333badf14c8c70d47c0a60d699bcd02df3bd83
kevinboone <kevin@railwayterrace.com>
2020-01-05 11:16:34 +0000

    Tidied up docs

--- kb_web_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-WEB=/home/kevin/docs/kzone5/
-SOURCE=$WEB/source
-TARGET=$WEB/target
-make clean
-(cd ..; tar cvfz $TARGET/dirsize.tar.gz dirsize)
-cp doc/README_dirsize.html $SOURCE
-(cd $WEB; ./make.pl README_dirsize)
